7f8599e211944148823f994159bd7dcab83f2f74
ivanpudin <ivanpudin9@gmail.com>
2023-04-24 10:58:47 +0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-#Animals app
+# Animals
+
+App created to try handling events, changing states, and how to style components using React
+
+# Technologies used
+
+- React
+
+# Features
+
+- Animals list:
+  display, like, remove and search animals
+
+-Birds list:
+display, like, remove and search birds
+
+# Getting started
+
+Clone repository
+
+Run frontend
+
+```
+npm start
+```
+
+# Credits
+
+[Unsplash](https://unsplash.com) for pictures
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,20 @@
 import { NavLink } from 'react-router-dom'
 import '../UI/Home.css'
+import animals from '../content/animals.jpg'
+import birds from '../content/birds.jpg'
 
 const Home = () => {
     return (
         <div className="Home">
             <NavLink to='/Animals'>
                 <div className='home_animals'>
+                    <img src={animals} alt="wolf" />
                     <h2>Animals</h2>
                 </div>
             </NavLink>
             <NavLink to='/Birds'>
                 <div className='home_birds'>
+                    <img src={birds} alt="parrot" />
                     <h2>Birds</h2>
                 </div>
             </NavLink>
